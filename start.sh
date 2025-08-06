40b768c03ea5e11c24addc85dbf2c0a343c0d055
duckbarii <duckduckkimaarandi@duck.com>
2025-08-06 15:46:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 # Update & upgrade system
-apt update && apt upgrade -y
-
-apt install git -y           
-pip install -U pip    
-pip install -U -r requirements.txt --force-reinstall
-
-# Start bot
-echo "Starting Bot...."
 python3 bot.py
