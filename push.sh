8540fb45cb1d0b0ef2e49a3d9a86039040ca6da4
duckbarii <duckduckkimaarandi@duck.com>
2025-08-06 11:25:12 +0530

    Initial commit

--- push.sh
+++ push.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Git user info
+read -sp "Enter your GitHub token: " ACCESS_TOKEN
+echo
+
 git config --global user.email "duckduckkimaarandi@duck.com"
 git config --global user.name "duckbarii"
-
-# Mark this directory as safe
 git config --global --add safe.directory "$(pwd)"
 
-# GitHub repo info
 REPO_NAME="Api"
 USERNAME="duckbarii"
-ACCESS_TOKEN="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"
 REPO_URL="https://${ACCESS_TOKEN}@github.com/${USERNAME}/${REPO_NAME}.git"
 
-# Git operations
 echo "# $REPO_NAME" > README.md
 git init
 git branch -M main
